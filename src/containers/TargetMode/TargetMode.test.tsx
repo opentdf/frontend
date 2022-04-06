59f75c9dd001ffce642f4d99c6ae8bedfea7e206
obidenko <obidenko@virtru.com>
2022-04-06 17:00:53 +0200

    Test cleanup
    Signed-off-by: obidenko <obidenko@virtru.com>

--- src/containers/TargetMode/TargetMode.test.tsx
+++ src/containers/TargetMode/TargetMode.test.tsx
@@ -1,49 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { InputTDF } from "./InputTDF";
-const { ReactKeycloakProvider } = require('@react-keycloak/web')
-
-// TODO mock tdf3 lib
-// jest.mock(
-//     '@opentdf/client',
-//     () => {
-//         const withStringSource = function () {
-//             return {
-//                 withOffline: jest.fn().mockReturnValue({
-//                     build: jest.fn()
-//                 })
-//             };
-//         };
-//
-//         const EncryptParamsBuilder = function () {
-//         };
-//         EncryptParamsBuilder.prototype.withStringSource = withStringSource;
-//
-//         const Client = function () {
-//         };
-//
-//         Client.prototype.encrypt = async function () {
-//             return Promise.resolve("Encrypt");
-//         }
-//
-//         Client.prototype.decrypt = function () {
-//             return Promise.resolve("Encrypt");
-//         }
-//
-//         return {
-//             Client: {
-//                 Client,
-//                 EncryptParamsBuilder,
-//                 DecryptParamsBuilder: jest.fn().mockImplementation(() => {
-//                     return {
-//                         withStringSource: jest.fn().mockImplementation(() => {
-//                             return { build: jest.fn() };
-//                         })
-//                     };
-//                 })
-//             }
-//         }
-//     }
-// );
+const { ReactKeycloakProvider } = require('@react-keycloak/web');
 
 describe('InputTDF component', () => {
     it("should render", () => {
