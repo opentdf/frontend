0ac6e8e54e7047d8fa6173ec1f53effba43eb7f1
Yustinian Sizariev <ysizariev@lohika.com>
2022-06-30 13:33:46 +0300

    fix: tests (#1896)

--- src/containers/Authorities/Authorities.test.tsx
+++ src/containers/Authorities/Authorities.test.tsx
@@ -1,39 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import Authorities from "./Authorities";
 import { BrowserRouter as Router } from "react-router-dom";
+import { setWindowMock, mockKeycloakStub } from "../../../tests/unit/helpers.mock";
+
 const { ReactKeycloakProvider } = require('@react-keycloak/web')
 
 describe('Authorities component', () => {
   it("is rendered", () => {
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest.fn().mockImplementation(query => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      })),
-    });
-
-    const mockKeycloakStub = {
-      init: jest.fn().mockResolvedValue(true),
-      login: jest.fn(),
-      logout: jest.fn(),
-      createLoginUrl: jest.fn(),
-      authenticated: true,
-      initialized: true
-    };
+    setWindowMock();
 
     render(
-      <ReactKeycloakProvider authClient={mockKeycloakStub}>
+      <ReactKeycloakProvider authClient={mockKeycloakStub()}>
         <Router>
           <Authorities />
         </Router>
-        </ReactKeycloakProvider>
+      </ReactKeycloakProvider>
     );
 
     const element = screen.getAllByText('Authority')[0] as HTMLElement;
--- src/containers/Header/Header.test.tsx
+++ src/containers/Header/Header.test.tsx
@@ -1,35 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
 import { BrowserRouter as Router } from "react-router-dom";
+import { setWindowMock, mockKeycloakStub } from "../../../tests/unit/helpers.mock";
 const { ReactKeycloakProvider } = require('@react-keycloak/web')
 
 describe('Header component', () => {
   it("is rendered", () => {
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest.fn().mockImplementation(query => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      })),
-    });
-
-    const mockKeycloakStub = {
-      init: jest.fn().mockResolvedValue(true),
-      login: jest.fn(),
-      logout: jest.fn(),
-      createLoginUrl: jest.fn(),
-      authenticated: true,
-      initialized: true
-    };
+    setWindowMock();
 
     render(
-      <ReactKeycloakProvider authClient={mockKeycloakStub}>
+      <ReactKeycloakProvider authClient={mockKeycloakStub()}>
         <Router>
           <Header />
         </Router>
