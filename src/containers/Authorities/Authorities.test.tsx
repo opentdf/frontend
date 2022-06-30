b3e0ac078810fcd3f21a4db0f5d32afb5b025807
Yustinian Sizariev <ysizariev@lohika.com>
2022-06-30 14:08:12 +0300

    fix: tests (#1896)

--- src/containers/Authorities/Authorities.test.tsx
+++ src/containers/Authorities/Authorities.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Authorities from "./Authorities";
 import { BrowserRouter as Router } from "react-router-dom";
-import { setWindowMock, mockKeycloakStub } from "../../../tests/unit/helpers";
+import { setWindowMock, mockKeycloakStub } from "../../../tests/unit";
 
 const { ReactKeycloakProvider } = require('@react-keycloak/web')
 
