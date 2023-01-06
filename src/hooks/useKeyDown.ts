c64729bfb9e511f1020d6d07cc6870130c3471e1
Paul Flynn <pflynn@virtru.com>
2023-01-06 10:51:14 -0500

    useCallback

--- src/hooks/useKeyDown.ts
+++ src/hooks/useKeyDown.ts
@@ -3,10 +3,9 @@ import { useEffect } from 'react';
 type Handler = ({ key }: { key: string }) => void;
 
 export const useKeyDown = (handler: Handler) => {
-    const keyDownHandler = (event: KeyboardEvent) => handler({ key: event.key });
     useEffect(() => {
+        const keyDownHandler = (event: KeyboardEvent) => handler({ key: event.key });
         document.addEventListener('keydown', keyDownHandler);
-
         return () => document.removeEventListener('keydown', keyDownHandler);
-    }, [keyDownHandler]);
+    }, [handler]);
 };
