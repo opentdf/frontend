167a7f37ae60f0f6761e8bfdcfa5babaff78dec0
Paul Flynn <pflynn@virtru.com>
2023-01-09 11:22:05 -0500

    react route fix

--- server/main.go
+++ server/main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -51,7 +50,7 @@ func main() {
 	}
 	// replace %REACT_APP_SERVER_DATA% in index file
 	m := regexp.MustCompile("%REACT_APP_SERVER_DATA%")
-	input, err := ioutil.ReadFile(filepath.Join(directory, index))
+	input, err := os.ReadFile(filepath.Join(directory, index))
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -93,8 +92,11 @@ type IndexHandler struct {
 func (h IndexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Security-Policy", "frame-ancestors")
     w.Header().Set("X-Content-Type-Options", "nosniff")
-	if r.URL.Path == "/" || r.URL.Path == "/index.html" {
-		w.Write(h.output)
+	if r.URL.Path == "/" || r.URL.Path == "/index.html"	|| r.URL.Path == "/authorities"	|| r.URL.Path == "/attributes"	|| r.URL.Path == "/entitlements" {
+		_, err := w.Write(h.output)
+		if err != nil {
+			return
+		}
 		return
 	}
 	h.fs.ServeHTTP(w, r)
