2eb9e6fba7dcc61769fab0a4be9397a3f9ca566b
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2023-07-20 18:44:37 +0300

    Ability to add/remove order values

--- src/components/EditValueList/EditValueList.tsx
+++ src/components/EditValueList/EditValueList.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
-import { Form, Input } from 'antd';
+import React, {FC, useEffect, useState} from 'react';
+import { Form, Input, Button } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
 
 type Props = {
     list: string[];
@@ -8,29 +9,69 @@ type Props = {
 
 const EditValueList: FC<Props> = (props) => {
     const [form] = Form.useForm();
-    const { list, onEdit } = props;
+    const { onEdit } = props;
+    const [list, setList] = useState<string[]>(props.list);
+
+    useEffect(() => {
+        onEdit(list.filter((item: string) => !!item));
+    }, [list]);
 
     const onBlurHandle = () => {
-        onEdit(Object.values(form.getFieldsValue()));
+        setList(form.getFieldsValue().values);
     };
 
     return (
-        <Form
-            form={form}
-            autoComplete="off"
-            initialValues={{ ...list }}
-            onBlur={onBlurHandle}
-        >
-            {list.map((item, index) => (
-                <Form.Item
-                    key={item}
-                    name={index}
-                    rules={[{ required: true, message: 'Order value should not be blank' }]}
-                >
-                    <Input id={'edit-value-input-field'}/>
-                </Form.Item>
-            ))}
-        </Form>
+      <Form
+        form={form}
+        name="form"
+      >
+          <Form.List
+            name="values"
+            initialValue={props.list}
+          >
+              {(fields, { add, remove }, { errors }) => (
+                <>
+                    {fields.map((field, index) => (
+                      <Form.Item
+                        required={false}
+                        key={field.key}
+                      >
+                          <Form.Item
+                            noStyle
+                            {...field}
+                            rules={[{ required: true, message: 'Order value should not be blank' }]}
+                          >
+                              <Input
+                                id={'edit-value-input-field'}
+                                onBlur={onBlurHandle}
+                                style={{
+                                    width: '98%',
+                                }}
+                              />
+                          </Form.Item>
+
+                          <DeleteOutlined
+                            style={{
+                                width: '2%',
+                            }}
+                            onClick={() => {
+                                remove(field.name);
+                                setList(form.getFieldsValue().values);
+                            }}
+                          />
+                      </Form.Item>
+                    ))}
+                    <Form.Item>
+                        <Button
+                          onClick={() => add()}
+                        >
+                            Add field
+                        </Button>
+                    </Form.Item>
+                </>
+              )}
+          </Form.List>
+      </Form>
     );
 }
 // Blocking unnecessary re render because we losing validation messages after it
